332d6f847b9994c8f834ecb3590c8cb8b277c513
jaywant3 <narwadejaywant@gmail.com>
2019-09-25 12:23:18 -0400

    Updates

---
+++ precache-manifest.3debc4b9c7cf7d9c66d27bf26cf8ac9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b4fb1a102b02becf9f325714e8edbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e70129752d36695542",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f544f03d94fc7607bf11",
+    "url": "/static/js/2.b7870c85.chunk.js"
+  },
+  {
+    "revision": "58e70129752d36695542",
+    "url": "/static/js/main.b118da16.chunk.js"
+  },
+  {
+    "revision": "0f6869ff3f362bcbfe9b",
+    "url": "/static/js/runtime-main.0d1b7ab8.js"
+  }
+]);
